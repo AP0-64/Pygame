6c26b1d12d2f2a7a8730b28bac634f7b9db00843
Bertini <bertin_p@etna-alternance.net>
2025-05-17 21:20:48 +0200

    ajout de la gravité

--- thune.py
+++ thune.py
@@ -5,28 +5,38 @@ pg.init()
 screen = pg.display.set_mode((540, 960))
 pg.display.set_caption("Test Pygame")
 
-TOTAL_FRAMES = 60 * 61
+clock = pg.time.Clock()
 
 
-class Balle:
+class Ball:
     def __init__(self):
         self.ball_position = [270, 480]
         self.ball_velocity = [0, 0]
+
+    def update(self):
+        self.ball_velocity[1] += 0.3
+        self.ball_position[0] += self.ball_velocity[0]
+        self.ball_position[1] += self.ball_velocity[1]
 
     def draw(self, screen):
         pg.draw.circle(screen, (255, 255, 255), self.ball_position, 44)
         pg.draw.circle(screen, (255, 0, 0), self.ball_position, 40)
 
 
-balle = Balle()
+ball = Ball()
 
 running = True
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+
+    ball.update()
+
     screen.fill((0, 0, 0))
-    balle.draw(screen)
+    ball.draw(screen)
     pg.display.flip()
+
+    clock.tick(60)
 
 pg.quit()

0978d0116a35d4a70f5e8de3654c0997ed39b059
Bertini <bertin_p@etna-alternance.net>
2025-05-17 21:10:35 +0200

    création de la classe Balle

--- thune.py
+++ thune.py
@@ -3,18 +3,30 @@ import pygame as pg
 pg.init()
 
 screen = pg.display.set_mode((540, 960))
-screen.fill((0, 0, 0))
 pg.display.set_caption("Test Pygame")
 
-pg.draw.circle(screen, (255, 255, 255), [270, 480], 44)
-pg.draw.circle(screen, (255, 0, 0), [270, 480], 40)
+TOTAL_FRAMES = 60 * 61
 
-pg.display.flip()
+
+class Balle:
+    def __init__(self):
+        self.ball_position = [270, 480]
+        self.ball_velocity = [0, 0]
+
+    def draw(self, screen):
+        pg.draw.circle(screen, (255, 255, 255), self.ball_position, 44)
+        pg.draw.circle(screen, (255, 0, 0), self.ball_position, 40)
+
+
+balle = Balle()
 
 running = True
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+    screen.fill((0, 0, 0))
+    balle.draw(screen)
+    pg.display.flip()
 
 pg.quit()

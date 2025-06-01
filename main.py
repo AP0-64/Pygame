38c564630f28365146af8ea209478ffcbcecd84f
Paolo BERTINI <bertin_p@etna-alternance.net>
2025-06-01 23:05:38 +0200

    note est jouer mais c pas ouf

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pretty_midi as pm
 import pygame as pg
+import pygame.midi as pgm
 
 pg.init()
+pgm.init()
 
 screen = pg.display.set_mode((1080, 1920))
 center = np.array([x // 2 for x in screen.get_size()])
@@ -14,6 +16,7 @@ clock = pg.time.Clock()
 midi_data = pm.PrettyMIDI("I'm Blue.mid")
 all_notes = [n for inst in midi_data.instruments for n in inst.notes]
 all_notes.sort(key=lambda n: n.start)
+midi_out = pgm.Output(0)
 
 
 class Ball:
@@ -21,6 +24,8 @@ class Ball:
         self.position = np.array([500, center[1]], dtype=float)
         self.velocity = np.array([0.0, 0.0])
         self.note_index = 0
+        self.note_end_time = 0
+        self.current_note = None
 
     def update(self):
         self.velocity[1] += 0.3
@@ -45,6 +50,12 @@ class Ball:
                 - 2 * np.dot(self.velocity, normal) * normal
             )
             self.position = center + normal * (300 - 44)
+            if self.note_index < len(all_notes):
+                note = all_notes[self.note_index]
+                midi_out.note_on(note.pitch, note.velocity)
+                self.note_index += 1
+                self.note_end_time = pg.time.get_ticks() + 300
+                self.current_note = note.pitch
 
 
 ball = Ball()
@@ -60,6 +71,9 @@ while running:
     ball.bounce()
     ball.update()
     ball.draw(screen)
+    if ball.current_note and pg.time.get_ticks() > ball.note_end_time:
+        midi_out.note_off(ball.current_note, 100)
+        ball.current_note = None
     pg.draw.circle(screen, (255, 255, 255), center.astype(int), 300, 10)
 
     pg.display.flip()
